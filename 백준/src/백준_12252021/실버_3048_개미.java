5e4bf3ae31d9528bcf8b107871a89edd77d44788
leen6225 <leen6225@gmail.com>
2021-12-27 23:24:52 +0900

    <백준>(실버4)3048_개미
    12/25 개미 실패 -> 12/27 개미 성공
    -수정 사항 : swap_index를 -1로 초기화 시킴
    swap_index의 default가 0으로 지정되기때문에 항상 맨 앞의 개미가 swap되어 로직과 다른 결과가 나왔다.

--- 백준/src/백준_12252021/실버_3048_개미.java
+++ 백준/src/백준_12252021/실버_3048_개미.java
@@ -7,6 +7,12 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class 실버_3048_개미 {
+	/**Ants
+	 * name : 개미의 letters
+	 * group : 
+	 * 		-group1 - 초기 상태에서 우측으로 가고있는 개미들
+	 * 		-group2 - 초기 상태에서 좌측으로 가고있는 개미들
+	 * */
 	public static class Ants {
 		char name;
 		int group;
@@ -17,47 +23,45 @@ public class 실버_3048_개미 {
 		}
 	}
 
-	public static ArrayList<Ants> ants;
-	public static String input2;
+	public static ArrayList<Ants> ants; //ArrayList에 개미의 정보를 담는다
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		br.readLine();// arraylist쓸거라 인풋 저장 안할거임
+		
+		br.readLine();// arraylist를 사용하기 때문에 따로 input의 사이즈는 저장하지 않고 넘긴다
+		
 		String input1 = br.readLine();
-		input2 = br.readLine();
+		String input2 = br.readLine();
 		int T = Integer.parseInt(br.readLine());
 		br.close();
+		
+		//ArrayList에 input으로 받은 개미들 저장
 		ants = new ArrayList<>();
 
-		for (int i = input1.length() - 1; i >= 0; i--)
-			ants.add(new Ants(1, input1.charAt(i)));
-		for (int i = 0; i < input2.length(); i++) {
-			ants.add(new Ants(2, input2.charAt(i)));			
-		}
+		for (int i = input1.length() - 1; i >= 0; i--)ants.add(new Ants(1, input1.charAt(i)));
+		for (int i = 0; i < input2.length(); i++) ants.add(new Ants(2, input2.charAt(i)));
+		
 
 		// 구현
 		// 본인 index의 group이 1이고, index+1의 group이 2인 경우를 저장
-		int[] swap_index;
+		int[] swap_index;//점프할 개미들의 index를 저장할 배열
 		while (T > 0) {
-			// String swap_index ="";//이렇게하면 10이 넘어갈때 오류생김
+			//초기화
 			swap_index = new int[ants.size()];
-
+			for (int i = 0; i < swap_index.length; i++) swap_index[i] = -1; 
 			int index_cnt = 0;
+			
+			//앞의 개미가 다른 그룹의 개미일 경우 swap_index에 현재 개미의 위치 저장
 			for (int i = 0; i < ants.size(); i++) {
-				if (encounter(i))
-					swap_index[index_cnt] = i;
+				if (encounter(i))swap_index[index_cnt] = i;
 				index_cnt++;
 			}
-			// System.out.println(swap_index);
 
+			//저장된 위치들의 개미와 위치+1의 개미들을 서로 swap
 			for (int i = 0; i < index_cnt; i++) {
-				swap(swap_index[i]);
+				if (swap_index[i] >= 0) swap(swap_index[i]);	
 			}
-			for (int i = 0; i < ants.size(); i++) {
-				System.out.print(ants.get(i).name);
-			}
-			System.out.println();
-			//if(done())break;
 			
 			T--;
 		}
@@ -70,6 +74,11 @@ public class 실버_3048_개미 {
 		bw.close();
 	}
 
+	/**Encounter(index)
+	 * 서로 다른 그룹의 개미를 만났을 때, 
+	 * 현재 index의 개미가 1그룹의 개미라면 return true
+	 * else false
+	 */
 	public static boolean encounter(int i) {
 		if (i == ants.size() - 1)
 			return false;// 맨 마지막 개미일때는 비교할것 없음
@@ -80,7 +89,8 @@ public class 실버_3048_개미 {
 		return false;
 	}
 
-	/**
+	/**Swap(index)
+	 * ants(index)와 ants(index+1)의 개미의 위치를 바꾸는 메서드
 	 * swap A and B
 	 */
 	public static void swap(int index) {
@@ -91,15 +101,5 @@ public class 실버_3048_개미 {
 		ants.get(index + 1).group = 1;
 
 	}
-	public static boolean done() {
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0 ; i<input2.length() ;i++) sb.append(ants.get(i).name);
-		System.out.println(sb.toString());
-		if(input2==sb.toString()) {
-			System.out.println("???");
-			return true;
-		}
-			
-		else return false;
-	}
+
 }
