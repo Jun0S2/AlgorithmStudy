5b1a1a6c1f2079be60ef589ed5f78bd7fc91e7bf
leen6225 <leen6225@gmail.com>
2022-01-05 00:52:07 +0900

    <프로그래머스>(레벨2)오픈체팅방
    initial approach : user 클래스에 nickname과 id를 둘 다 저장하여 중복일 경우 for loop를  사용하여 로그 수정 -> 시간초과
    fix : user클래스에는 아이디와 메시지만 관리하고, 해쉬맵에 닉네임과 아이디를 따로 관리하여 answer 배열에 로그에 저장된 아이디로 해쉬맵에서 닉네임을 얻어옴

--- 프로그래머스/src/프로그래머스_01042022/오픈채팅방.java
+++ 프로그래머스/src/프로그래머스_01042022/오픈채팅방.java
@@ -1,50 +1,50 @@
 package 프로그래머스_01042022;
-/**
- * 알고리즘 스터디
- * 정답 실패
- * -> 해쉬맵으로 풀어보라고 피드백 받음
- * */
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class 오픈채팅방 {
 
 	public static void main(String[] args) {
-		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
-		solution(record);
+		String[] record = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo",
+				"Change uid4567 Ryan" };
+		System.out.println(solution(record));
 	}
 
-	public static ArrayList<User> logs = new ArrayList<>();
-	public static ArrayList<String> messages = new ArrayList<>();
-	
-	public static class User{
+	public static class User {
 		String userid;
-		boolean visited;
-		String nickname;
-		
-		public User(String userid, String nickname) {
-			this.nickname = nickname;
-			this.userid = userid;
-			this.visited = true;
-		}
+		String msg;
 
-	}
-	
-	private static void solution(String[] record) {
-		/*Step 1 : records 에서 기록 분리*/
-		for(int i = 0 ; i<record.length ; i++) {
-			String[] temp= record[i].split(" ");
-			if(temp[0]=="Enter" && !logs.contains(temp[1])) {
-				logs.add(new User(temp[1],temp[2]));	
-			}	
+		public User(String userid, String msg) {
+			this.userid = userid;
+			this.msg = msg;
 		}
+	}
+
+	private static String[] solution(String[] record) {
+		ArrayList<User> log = new ArrayList<>();//닉네임이 아니라 userid를 저장하는 형식으로!
+		HashMap<String, String> nickname = new HashMap<>();//아이디, 닉네임
 		
-		for(int i = 0 ; i<record.length ; i++) {
-			if(!logs.isEmpty()) {
-				System.out.println(logs.get(i).userid);
+		for (int i = 0; i < record.length; i++) {
+			String temp[] = record[i].split(" ");
+
+			if (temp[0].equals("Enter")) {	
+				nickname.put(temp[1], temp[2]);
+				log.add(new User(temp[1], "님이 들어왔습니다.")); 
+			} else if (temp[0].equals("Change")) {
+				nickname.put(temp[1], temp[2]);	
+			} else if (temp[0].equals("Leave")) {
+				log.add(new User(temp[1],"님이 나갔습니다."));		
 			}
 		}
-		
-		
+
+		String[] answer = new String[log.size()];
+		for(int i = 0 ; i<answer.length;i++) {
+			answer[i] = nickname.get(log.get(i).userid)+log.get(i).msg;
+			//System.out.println(answer[i]);
+			}
+		return answer;
+
 	}
 
 }
