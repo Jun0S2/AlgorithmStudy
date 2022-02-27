8a9a62da7b8fcd8cd6057fcc77f31355f7efb77b
leen6225 <leen6225@gmail.com>
2022-02-28 00:13:28 +0900

    <프로그래머스>(LV1) 약수의_개수와_덧셈

---
+++ 프로그래머스/src/프로그래머스_02272022/약수의_개수와_덧셈.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for(int i = left ; i<=right ; i++)
+    {
+        int cnt = 0;//약수의 개수 카운트
+        for(int j = 1 ; j<= i ; j++)
+        {
+            if(i%j==0)cnt++;
+        }
+        if(cnt%2==0) answer += i;
+        else answer -= i;
+    }
+    return answer;
+}
