2c35c0c4269a1c409e0beb2fca266e0ba919383e
leen6225 <leen6225@gmail.com>
2022-03-02 01:19:15 +0900

    <백준>(브론즈)팩토리얼
    [문제번호]10872

---
+++ 백준/src/백준_03022022/브론즈_팩토리얼.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n==0||n==1) return 1;
+    else return n*factorial(n-1);
+}
+
+int main(){
+    int N;
+    cin>>N;
+    cout<<factorial(N)<<endl;
+}
