5b6a55b23f77bedce306169b11c63f357b44efdc
leen6225 <leen6225@gmail.com>
2022-03-07 12:12:37 +0900

    <백준>(실버)스위치켜고끄기 [문제번호]1244

--- 백준/src/백준_03052022/스위치켜고끄기.cpp
+++ 백준/src/백준_03052022/스위치켜고끄기.cpp
@@ -7,18 +7,15 @@ using namespace std;
 int N;
 vector<int> lights(N + 1, 2);
 
-//남학생 : 자기와 받은 수의 배수이면 스위치 상태 바꿈
+/* 남학생 : 자기와 받은 수의 배수이면 스위치 상태 바꿈*/
 void Male(int n)
 {
-    for (int i = n; i <= N; i += n)
+    for (int i = n; i <= N; i += n) // 배수만 switch
     {
-        if (lights[i] == 0)
-            lights[i] = 1;
-        else
-            lights[i] = 0;
+        lights[i] = lights[i] == 0 ? 1 : 0;
     }
 }
-
+/* 여학생 : 스위치를 중심으로 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간*/
 void Female(int n)
 {
 
@@ -26,51 +23,44 @@ void Female(int n)
 
     for (int index = 1; index <= N; index++)
     {
-        if (n - index == 0)
-            break;
-        cout << n - index << " and " << n + index << " ";
-        if (lights[n - index] == lights[n + index])
+        if (n - index == 0||n + index > N)  break; //범위 out
+
+        if (lights[n - index] == lights[n + index])//대칭이면 switch
         {
             lights[n - index] = lights[n - index] == 0 ? 1 : 0; // change status
             lights[n + index] = lights[n + index] == 0 ? 1 : 0; // change status
         }
-        else
-            break;
+        else break;
     }
 }
 
-// 1~8 on or off
 int main()
 {
-    //여학생 : 스위치를 중심으로 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간
     cin >> N; //스위치의 개수
-    lights.resize(N, 0);
+    lights.resize(N+1, 0);//배열 리사이즈 just in case
 
-    for (int i = 1; i <= N; i++)
-        cin >> lights[i];
+    for (int i = 1; i <= N; i++) cin >> lights[i]; //input
 
-    cout << "INPUT : ";
-    cout << endl;
-    for (int j = 1; j <= N; j++)
-        cout << lights[j] << " ";
-    cout << endl;
-
-    int M;
+    int M ,gender, num;
     cin >> M;
-    int male, female;
+    
     for (int i = 0; i < M; i++)
     {
-        cin >> male >> female;
-        Male(male);
-        cout << "BOYDS : ";
-        for (int j = 1; j <= N; j++)
-            cout << lights[j] << " ";
-        cout << endl;
-        cout << endl;
-        Female(female);
-        cout << "GALS: ";
-        for (int j = 1; j <= N; j++)
-            cout << lights[j] << " ";
+        cin >> gender >> num;
+        if(gender==1) Male(num);
+        else Female(num);
     }
+
     //출력 : 한줄에 20개까지 출력
+    int cnt =  0;
+    for(int j = 1; j <=N; j++)
+    {
+      ++cnt;
+      if(cnt==20)
+      {
+          cout<<lights[j]<<endl;
+          cnt = 0;
+      }
+      else cout<<lights[j]<<" ";
+    }
 }
