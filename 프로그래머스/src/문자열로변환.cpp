d1ffa74b2bed61f3c90d1ee3c154068b734b9264
June Park <36508552+Jun0S2@users.noreply.github.com>
2024-11-07 17:53:41 +0900

    Create 문자열로변환.cpp

---
+++ 프로그래머스/src/문자열로변환.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = to_string(n);  // n을 문자열로 변환
+    return answer;
+}
