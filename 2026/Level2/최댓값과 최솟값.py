3c7ae02394263b97bf66ef84493d303006e62ad3
jun0S2 <leen6225@gmail.com>
2026-05-05 22:34:58 +0200

    Level2

---
+++ 2026/Level2/최댓값과 최솟값.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=python3
+def solution(s):
+    nums = list(map(int, s.split()))
+    return f"{min(nums)} {max(nums)}"
+
+# s.split() → "1 2 3 4" → ["1", "2", "3", "4"]
+# map(int, arr) → [1, 2, 3, 4]
+# min(nums) → 최소값
+# max(nums) → 최대값
+# f"{min} {max}" → 문자열로 합치기
